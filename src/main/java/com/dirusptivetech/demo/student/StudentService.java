06380c49cd0b1d7a6ef21da947defc6db63550f5
Guy Bertrand <guybertrand.wandji@gmail.com>
2023-04-28 21:53:32 +0200

    Implement Put and delete metho api

--- src/main/java/com/dirusptivetech/demo/student/StudentService.java
+++ src/main/java/com/dirusptivetech/demo/student/StudentService.java
@@ -1,11 +1,9 @@
 package com.dirusptivetech.demo.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,5 +28,31 @@ public  class StudentService {
         }
 
         this.studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        Student existingStudent = this.studentRepository.getReferenceById(studentId);
+        if (!this.studentRepository.existsById(studentId) )
+            throw new IllegalStateException("Student not exists");
+        this.studentRepository.delete(existingStudent);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+
+            Optional<Student> studentOptional = this.studentRepository.findById(studentId);
+            if (studentOptional.isPresent()){
+                if (email!=null && email.length()> 0){
+                    Optional stOpt = this.studentRepository.findStudentByEmail(email);
+                    if (stOpt.isPresent()){
+                        throw new IllegalStateException("Email already taken");
+                    }
+                    studentOptional.get().setEmail(email);
+                }
+                if (name != null && name.length() > 0)
+                    studentOptional.get().setName(name);
+            }
+            else {throw new IllegalStateException("Student doesn't exist");}
+
     }
 }
